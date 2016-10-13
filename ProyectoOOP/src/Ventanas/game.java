19f21f16a84535f2a84180bea0135be31f555848
Steven-Peraza <perazas376@gmail.com>
2016-10-13 15:25:02 -0600

    Mejoras Visuales en Ventana Estadisticas de Grupo

--- ProyectoOOP/src/Ventanas/game.java
+++ ProyectoOOP/src/Ventanas/game.java
@@ -803,7 +803,7 @@ public class game extends javax.swing.JFrame {
              
             }
 
-            } else {
+             else {
                 
                     
                 if ((ProyectoOOP.counter<ProyectoOOP.grp.getMenorMovimientos())){
@@ -831,7 +831,7 @@ public class game extends javax.swing.JFrame {
                         
                 }
 
-        
+        }
         clip3.stop();
         panel.setFocusable(true);
     }//GEN-LAST:event_siguienteActionPerformed
